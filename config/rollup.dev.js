468ffc7f54d10757aea6192d70aa2cc3aa0a73c3
rista404 <risticn6@gmail.com>
2016-09-01 22:50:30 +0200

    Adds support for importing external modules from node_modules

--- config/rollup.dev.js
+++ config/rollup.dev.js
@@ -1,14 +1,18 @@
 import babel from 'rollup-plugin-babel'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
 	entry: 'src/js/main.js',
 	dest: 'dist/bundle.min.js',
 	format: 'iife',
 	plugins: [
+		commonjs(),
+		nodeResolve(),
 		babel({
 			exclude: 'node_modules/**',
 			presets: 'es2015-rollup',
 			plugins: ['transform-object-rest-spread']
-		})	
+		})
 	]
 }
--- config/rollup.prod.js
+++ config/rollup.prod.js
@@ -1,11 +1,15 @@
 import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
 	entry: 'src/js/main.js',
 	dest: 'dist/bundle.min.js',
 	format: 'iife',
 	plugins: [
+		commonjs(),
+		nodeResolve(),
 		babel({
 			exclude: 'node_modules/**',
 			presets: 'es2015-rollup',
